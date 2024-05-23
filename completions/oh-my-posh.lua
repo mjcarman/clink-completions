303069b17602b64d0ace60092f6267d6406fc531
Michael Carman <michael.j.carman@gmail.com>
2024-05-22 21:02:59 -0500

    Add flags and flag arguments for oh-my-posh.

--- completions/oh-my-posh.lua
+++ completions/oh-my-posh.lua
@@ -4,10 +4,10 @@ require("arghelper")
 local file_matches = clink.argmatcher():addarg(clink.filematches)
 
 local global_flags = ({
-  { hide=true, "-h"                                                       },
-  {            "--help",                             "help for command"   },
-  { hide=true, "-c"       ..file_matches                                  },
-  {            "--config" ..file_matches,  " <file>", "config (required)" },
+  { hide=true, "-h"                                                      },
+  {            "--help",                             "help for command"  },
+  { hide=true, "-c"       ..file_matches                                 },
+  {            "--config" ..file_matches,  " <file>", "config file path" },
 })
 
 
@@ -41,8 +41,10 @@ local config_parser = clink.argmatcher()
 local debug_parser = clink.argmatcher()
   :_addexflags({
     global_flags,
-    { "--pwd",               "current working directory" },
-    { "--shell", " <shell>", "the shell to print for" },
+    { "-p", hide=true },
+    { "--plain",              "plain text output (no ANSI)" },
+    { "--pwd",   " <string>", "current working directory"   },
+    { "--shell", " <shell>",  "the shell to print for"      },
   })
 
 
@@ -50,14 +52,19 @@ local font_parser = clink.argmatcher()
   :addarg(
     "install",
     "configure")
-  :_addexflags(global_flags)
+  :_addexflags({
+    global_flags,
+    { "--user", "install font as user" },
+  })
 
 
 local get_parser = clink.argmatcher()
   :addarg(
     "shell",
     "millis",
-    "accent")
+    "accent",
+    "toggles",
+    "width")
   :_addexflags({
     global_flags,
     { "--shell", " <shell>", "the shell to print for" },
@@ -93,17 +100,23 @@ local print_parser = clink.argmatcher()
     "error")
   :_addexflags({
     global_flags,
-                         { "--command",        "tooltip command"                               },
-    { "-e", hide=true},  { "--error",          "last exit code"                                },
-                         { "--eval",           "output the prompt for eval"                    },
-                         { "--execution-time", "timing of the last command"                    },
-    { "-p", hide=true }, { "--plain",          "plain text output (no ANSI)"                   },
-                         { "--pswd",           "current working directory (according to pwsh)" },
-                         { "--pwd",            "current working directory"                     },
-                         { "--shell",          "the shell to print for"                        },
-                         { "--shell-version",  "the shell version"                             },
-    { "-s", hide=true},  { "--stack-count",    "number of locations on the stack"              },
-    { "-w", hide=true }, { "--terminal-width", "width of the terminal"                         },
+                         { "--cleared",                     "do we have a clear terminal or not"                 },
+                         { "--column",         " <int>",    "the column position of the cursor"                  },
+                         { "--command",        " <string>", "tooltip command"                                    },
+    { "-e", hide=true},  { "--error",          " <int>",    "last exit code"                                     },
+                         { "--eval",                        "output the prompt for eval"                         },
+                         { "--execution-time", " <float>",  "timing of the last command"                         },
+                         { "--no-exit-code",                "no valid exit code (cancelled or no command yet)"   },
+                         { "--no-status",                   "no valid status code (cancelled or no command yet)" },
+                         { "--pipestatus",     " <string>", "the PIPESTATUS array"                               },
+    { "-p", hide=true }, { "--plain",                       "plain text output (no ANSI)"                        },
+                         { "--pswd",                        "current working directory (according to pwsh)"      },
+                         { "--pwd",                         "current working directory"                          },
+                         { "--shell",          " <string>", "the shell to print for"                             },
+                         { "--shell-version",  " <string>", "the shell version"                                  },
+    { "-s", hide=true},  { "--stack-count",    " <int>",    "number of locations on the stack"                   },
+                         { "--status",         " <int>",    "last known status code"                             },
+    { "-w", hide=true }, { "--terminal-width", " <int>",    "width of the terminal"                              },
   })
 
 
@@ -116,6 +129,8 @@ local prompt_parser = clink.argmatcher()
   :_addexflags(global_flags)
 
 
+local feature_parser = clink.argmatcher():addarg("notice")
+
 
 clink.argmatcher("oh-my-posh")
   :_addexarg({
@@ -123,12 +138,15 @@ clink.argmatcher("oh-my-posh")
     { "completion" .. completion_parser, "Generate the autocompletion script for the specified shell" },
     { "config"     .. config_parser,     "Interact with the config"                                   },
     { "debug"      .. debug_parser,      "Print the prompt in debug mode"                             },
+    { "disable"    .. feature_parser,    "Disable a feature"                                          },
+    { "enable"     .. feature_parser,    "Enable a feature"                                           },
     { "font"       .. font_parser,       "Manage fonts"                                               },
     { "get"        .. get_parser,        "Get a value from oh-my-posh"                                },
     { "help",                            "Help about any command"                                     },
     { "init"       .. init_parser,       "Initialize your shell and config"                           },
     { "print"      .. print_parser,      "Print the prompt/context"                                   },
     { "prompt"     .. prompt_parser,     "Set up the prompt for your shell (deprecated)"              },
+    { "toggle",    " <flags>",           "Toggle a segment on/off on the fly"                         },
     { "version",                         "Print the version"                                          },
   })
   :_addexflags({
